6d3c59fe41010656351ab31a1aa0c5114f99e61a
towsif031 <towsif031@gmail.com>
2020-04-25 00:43:07 +0600

    print file paths

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import os
 
-for (path, dirs, files) in os.walk('test_folder'):
-    print('path: ', path)
-    print('folders: ', dirs)
-    print('files: ', files)
-    print("\n")
+input_dir = "test_folder"
+
+# for i in os.walk(input_dir):
+#     print(i)
+
+file_list = []
+file_path_list = []
+for dirpath, dirnames, filenames in os.walk(input_dir):
+    for filename in filenames:
+        file_list.append(filename)
+
+    for file in file_list:
+        file_path = os.path.join(dirpath, file)
+        file_path_list.append(file_path)
+
+# print(file_list)
+
+print("\n".join(file_path_list))
