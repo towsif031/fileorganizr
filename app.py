24d334ad4d5e9203bfe4df53de372d6e4262f663
towsif031 <towsif031@gmail.com>
2020-04-26 12:32:02 +0600

    created function to search files by pattern

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
 import os
+import re
 
-input_dir = "test_folder"
 
-# for i in os.walk(input_dir):
-#     print(i)
+def directory_spider(input_dir, file_pattern="", maxResults=2):
+    file_list = []
+    file_path_list = []
+    for dirpath, dirnames, filenames in os.walk(input_dir):
+        for filename in filenames:
+            file_list.append(filename)
 
-file_list = []
-file_path_list = []
-for dirpath, dirnames, filenames in os.walk(input_dir):
-    for filename in filenames:
-        file_list.append(filename)
+        for file in file_list:
+            if re.search(file_pattern, file):
+                file_path = os.path.join(dirpath, file).replace("\\", "/")
+                file_path_list.append(file_path)
 
-    for file in file_list:
-        file_path = os.path.join(dirpath, file).replace("\\", "/")
-        file_path_list.append(file_path)
+        if len(file_path_list) > maxResults:
+            break
 
-# print(file_list)
+    return file_path_list[0:maxResults]
 
-print("\n".join(file_path_list))
+
+# input_dir = "test_folder"
+user_input_dir = input("Enter directory path: ")
+file_pattern = input("Enter file_pattern: ")
+maxResults = int(input("Number of results: "))
+print("\n")
+
+input_dir = user_input_dir.replace("\\", "/")
+output = directory_spider(input_dir, file_pattern, maxResults)
+print("\n".join(output))
